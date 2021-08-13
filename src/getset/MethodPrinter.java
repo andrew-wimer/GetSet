d885b2f110751cbaa2324760992467f5074c4219
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-13 05:25:40 -0400

    first version

---
+++ src/getset/MethodPrinter.java
@@ -0,0 +1,23 @@
+/**
+* PROGRAMMED BY: Andrew Wimer
+* CREATED ON: August 12 2021
+* LAST UPDATE: August 12 2021
+*/
+
+package getset;
+
+/**
+ * INTERFACE DESCRIPTION: An interface for a method printer object. 
+ * Method printer objects will print our methods/functions to either 
+ * a program file, the console, or to the user's clipboard.
+ * @author Andrew Wimer
+ */
+public interface MethodPrinter {
+   
+   public void printToFile(MethodQueue getters, MethodQueue setters);
+   
+   public void printToConsole(MethodQueue getters, MethodQueue setters);
+   
+   public void printToClipboard(MethodQueue getters, MethodQueue setters);
+
+}
