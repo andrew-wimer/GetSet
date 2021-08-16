d22d9134dc03d88b51f60b470547e28df4b72f20
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-15 22:30:13 -0400

    removed openFile method: file opening should be handled in parseFile method

--- src/getset/DataMemberParser.java
+++ src/getset/DataMemberParser.java
@@ -1,10 +1,12 @@
 /**
 * PROGRAMMED BY: Andrew Wimer
 * CREATED ON: August 12 2021
-* LAST UPDATE: August 12 2021
+* LAST UPDATE: August 15 2021
 */
 
 package getset;
+
+import java.io.FileNotFoundException;
 
 /**
  * INTERFACE DESCRIPTION: DataMemberParse parses class files for Data Members
@@ -13,14 +15,20 @@ package getset;
  */
 public interface DataMemberParser {
    
-   /**
-    * Opens file to read.
-    */
-   public void openFile();
+   
+   
    
    /**
     * Parses the file for data members to add to the DataMemberMap
     */
-   public void parseFile();
+   public DataMemberMap parseFile(String filePath) throws FileNotFoundException;
+   
+   /**
+    * Returns true if the parser is in the middle of comment block, or scans
+    * a comment line. 
+    * Return false when line is not part of comment
+    * @return 
+    */
+   public boolean isComment(String line);
 
 }
