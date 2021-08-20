327b577ac01f674873433f7ff7dd5c1e1d871e04
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-19 22:03:58 -0400

    initial version

---
+++ src/getset/GetSetProgram.java
@@ -0,0 +1,34 @@
+/**
+* PROGRAMMED BY: Andrew Wimer
+* CREATED ON: Aug 19 2021
+* LAST UPDATE: Aug 19 2021
+*/
+
+package getset;
+
+/**
+ * CLASS DESCRIPTION: Runs the GetSet program as per the String 
+ * arguments provided when it is called in the main method in GetSet.java
+ * @author Andrew Wimer
+ * 
+ */
+public class GetSetProgram {
+   
+   public GetSetProgram()
+   {
+      
+   }
+   
+   
+   public void start()
+   {
+      
+   }
+   
+   public void start(String progLanguage)
+   {
+      
+   }
+   
+
+}
