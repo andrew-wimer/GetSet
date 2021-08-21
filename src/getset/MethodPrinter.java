1c00792d8b96e87a18c6bf8a15855de15da27704
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-20 20:41:05 -0400

    moved printToConsole and copyToClipboard to MethodPrinter, which is now abstract.

--- src/getset/MethodPrinter.java
+++ src/getset/MethodPrinter.java
@@ -1,23 +1,65 @@
 /**
 * PROGRAMMED BY: Andrew Wimer
 * CREATED ON: August 12 2021
-* LAST UPDATE: August 15 2021
+* LAST UPDATE: August 20 2021
 */
 
 package getset;
 
+import java.io.FileNotFoundException;
+
 /**
- * INTERFACE DESCRIPTION: An interface for a method printer object. 
+ * Abstract Class Description: An interface for a method printer object. 
  * Method printer objects will print our methods/functions to either 
  * a program file, the console, or to the user's clipboard.
  * @author Andrew Wimer
+ * v2 Converted from interface to abstract class. Print to console 
+ * and copy to clipboard should universally function the same for
+ * each language, unlike printing to a file.
  */
-public interface MethodPrinter {
+abstract public class MethodPrinter {
    
-   public void printToFile(MethodQueue getters, MethodQueue setters);
+abstract public void printToFile(MethodQueue getters, MethodQueue setters,
+           String filePath) throws FileNotFoundException;
    
-   public void printToConsole(MethodQueue getters, MethodQueue setters);
+   /**
+    * Prints  a full field of getters and full field of setters. 
+    * @param getters
+    * @param setters
+    * @param isAlternating 
+    */
+   public void printToConsole(MethodQueue getters, MethodQueue setters) {
+      
+      while(getters.peek() != null)
+      {
+         System.out.println(getters.poll());
+      }
+      
+      while(setters.peek() != null)
+      {
+         System.out.println(setters.poll());
+      }
+   }
    
-   public void copyToClipboard(MethodQueue getters, MethodQueue setters);
+   /**
+    * Prints alternating setters and getters to the console instead of
+    * printing a full field of getters and full field of setters.
+    * @param getters
+    * @param setters
+    * @param isAlternating 
+    */
+   public void printToConsoleAlternating(MethodQueue getters, 
+           MethodQueue setters) {
+
+         while(getters.peek() != null && setters.peek() != null)
+         {
+            System.out.println(getters.poll());
+            System.out.println(setters.poll());
+         }
+   }
+
+   public void copyToClipboard(MethodQueue getters, MethodQueue setters) {
+      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   }
 
 }
