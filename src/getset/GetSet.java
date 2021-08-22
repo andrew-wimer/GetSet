50394063703b33b4b7381acac8c9e57910a11eff
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-21 22:22:18 -0400

    moved menu system to GetSetProgram class. GetSet.java will now serve as the entry/main method class for the Getter Setter Program

--- src/getset/GetSet.java
+++ src/getset/GetSet.java
@@ -3,14 +3,12 @@
 * PROGRAMMED BY: Andrew Wimer
 * DESCRIPTION: 
 * DATE CREATED: Aug 7 2021
-* LAST UPDATED: Aug 8 2021
+* LAST UPDATED: Aug 21 2021
 */
 
 package getset;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * GetSet writes setter and getter methods/functions in a specified language
@@ -18,31 +16,19 @@ import java.io.InputStreamReader;
  * As of V1, GetSet only writes Java methods, and only writes one method at a
  * time. (Aug 8 2021)
  * @author Andrew Wimer
+ * 
+ * Aug 21 2021 - moved menu system to GetSetProgram class. GetSet.java will
+ * now serve as the entry/main method class for the Getter Setter Program
  */
 public class GetSet {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
-       BufferedReader reader = new BufferedReader(
-            new InputStreamReader(System.in));
-       String id; // identifier
-       String returnType; //data type of identifier
+    public static void main(String[] args) throws IOException  {
+       GetSetProgram gsPrgrm = new GetSetProgram(args);
+       gsPrgrm.start();
        
-       System.out.println("Enter your identifier: ");
-       id = reader.readLine();
-       System.out.println("identifier  = " + id);
-       System.out.println("Enter your data type: ");
-       returnType = reader.readLine();
-       System.out.println("data type  = " + returnType);
-
-       Setter js = new JavaSetter();
-       Getter jg = new JavaGetter();
-       System.out.println(jg.writeGetter(id, returnType));
-        System.out.println(js.writeSetter(id, returnType));
-
-        reader.close();
     }
 
 }
