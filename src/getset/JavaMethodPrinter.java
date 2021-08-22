9a4e9d401125a434caa6203f79d3b9141617d88b
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-21 22:23:43 -0400

    first working version of printToFileBottom method

--- src/getset/JavaMethodPrinter.java
+++ src/getset/JavaMethodPrinter.java
@@ -1,23 +1,30 @@
 /**
 * PROGRAMMED BY: Andrew Wimer
 * CREATED ON: August 12 2021
-* LAST UPDATE: August 20 2021
+* LAST UPDATE: August 21 2021
 */
 
 package getset;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayDeque;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
  * CLASS DESCRIPTION: JavaMethodPrinter prints the queues of getter and setter
  * methods to either the console, the clipboard, or to a file.
  * @author Andrew Wimer
+ * aug 21 2021 - 
  * 
  */
 public class JavaMethodPrinter extends MethodPrinter {
@@ -38,7 +45,11 @@ public class JavaMethodPrinter extends MethodPrinter {
       String fileName) throws FileNotFoundException 
    {  
       Scanner scnr = new Scanner(new File(fileName));
-      printToFileBottom(getters, setters, fileName, scnr);
+      try {
+         printToFileBottom(getters, setters, fileName, scnr);
+      } catch (IOException ex) {
+         Logger.getLogger(JavaMethodPrinter.class.getName()).log(Level.SEVERE, null, ex);
+      }
 
    }  
    
@@ -51,7 +62,7 @@ public class JavaMethodPrinter extends MethodPrinter {
     */
       public void printToFile(MethodQueue getters, MethodQueue setters,
            String fileName, String placementLocation) 
-              throws FileNotFoundException
+              throws FileNotFoundException, IOException
       {
       
       Scanner scnr = new Scanner(new File(fileName));
@@ -67,20 +78,28 @@ public class JavaMethodPrinter extends MethodPrinter {
     * Prints getters and setters to the bottom of the specified file
     */
    private void printToFileBottom(MethodQueue getters, MethodQueue setters,
-           String fileName, Scanner fileScnr)
+           String fileName, Scanner fileScnr) throws IOException
    {
       List<String> fileAsList = new ArrayList();
-      ArrayDeque<String> braceStack = new ArrayDeque();
+      List<String> getterSetterList = new ArrayList();
+      int indexOfLastBrace = 0;
       
       while (fileScnr.hasNext())
       {
          String line = fileScnr.next();
          fileAsList.add(line);
       }
+      indexOfLastBrace = fileAsList.lastIndexOf("}");
+      getterSetterList = printSeparately(getters, setters ,fileAsList);
+      fileAsList.addAll(indexOfLastBrace, getterSetterList);
+      fileAsList.add("}");
+      
+      Path file = Paths.get(fileName);
+      Files.write(file, fileAsList, StandardCharsets.UTF_8);
    }
    
    /**
-    * Prints getters and setters to below the constructors in a file 
+    * Prints getters and setters to directly below the constructors in a file 
     */
    private void printBelowConstructors(MethodQueue getters, MethodQueue setters,
            String fileName, Scanner fileScnr)
@@ -88,14 +107,63 @@ public class JavaMethodPrinter extends MethodPrinter {
       throw new UnsupportedOperationException();
    }
    
-   
-   private void printConsecutively()
+   /**
+    * Prints getters and setters in two separate consecutive lists.
+    * @param getters
+    * @param setters
+    * @param fileAsList
+    * @return 
+    */
+   private List<String> printSeparately(MethodQueue getters, 
+           MethodQueue setters, List<String> fileAsList)
    {
+      while(getters.peek() != null)
+      {
+         fileAsList.add(getters.poll());
+      }
       
+      while(setters.peek() != null)
+      {
+         fileAsList.add(setters.poll());
+      }
+      return fileAsList;
    }
    
-   private void printAlternating()
+   /**
+    * Prints alternating between setters and getters.
+    * @param getters
+    * @param setters
+    * @param fileAsList
+    * @return 
+    */
+   private List<String> printAlternating(MethodQueue getters, 
+           MethodQueue setters, List<String> fileAsList)
    {
+      while(getters.peek() != null)
+      {
+         fileAsList.add(getters.poll());
+         fileAsList.add(setters.poll());
+      }
       
+      return fileAsList;
+   }
+   
+    /**
+    * Returns true if the line contains the characters // which precedes 
+    * comment lines, and *, which precedes javadoc comments
+    * @param line
+    * @return 
+    */
+   private boolean isComment(String line)
+   {
+      line = line.trim();
+      
+      return line.startsWith("//") || line.startsWith("*");
+   }
+   
+   
+   private boolean hasNonCommentBrace(String line)
+   {
+     throw new UnsupportedOperationException(); 
    }
 }
