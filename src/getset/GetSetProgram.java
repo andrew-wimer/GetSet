49a9449c2cf161597a712a84f4c0884e4a2dedaf
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-21 22:22:39 -0400

    moved menu system to GetSetProgram class. GetSet.java will now serve as the entry/main method class for the Getter Setter Program

--- src/getset/GetSetProgram.java
+++ src/getset/GetSetProgram.java
@@ -6,6 +6,10 @@
 
 package getset;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * CLASS DESCRIPTION: Runs the GetSet program as per the String 
  * arguments provided when it is called in the main method in GetSet.java
@@ -19,10 +23,31 @@ public class GetSetProgram {
       
    }
    
-   
-   public void start()
+   public GetSetProgram(String[] args)
    {
       
+   }
+   
+   public void start() throws IOException
+   {
+      BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+       String id; // identifier
+       String returnType; //data type of identifier
+       
+       System.out.println("Enter your identifier: ");
+       id = reader.readLine();
+       System.out.println("identifier  = " + id);
+       System.out.println("Enter your data type: ");
+       returnType = reader.readLine();
+       System.out.println("data type  = " + returnType);
+
+       Setter js = new JavaSetter();
+       Getter jg = new JavaGetter();
+       System.out.println(jg.writeGetter(id, returnType));
+        System.out.println(js.writeSetter(id, returnType));
+
+        reader.close();
    }
    
    public void start(String progLanguage)
