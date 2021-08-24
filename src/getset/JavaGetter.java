19f2cd632e73ea34fbde47c8dc6d4458a13c2210
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-23 23:54:16 -0400

    added writeFileFormattedGetter, which writes the method to be properly tabbed to be written directly to a formatted .java file

--- src/getset/JavaGetter.java
+++ src/getset/JavaGetter.java
@@ -14,11 +14,21 @@ package getset;
  */
 public class JavaGetter implements Getter {
    
+   /**
+    * Default constructor.
+    */
    public JavaGetter()
    {
       
    }
    
+   /**
+    * Generates a String form of a getter method, where only the return
+    * line is tabbed.
+    * @param identifier
+    * @param returnType
+    * @return 
+    */
    @Override
    public String writeGetter(String identifier, String returnType)
    {
@@ -30,6 +40,25 @@ public class JavaGetter implements Getter {
               + "()" + "\n{\n   return " + identifier + ";" + "\n}";
       
       return getterString;
-   }
+   }  
 
+   /**
+    * Generates a String form of a getter method, every lined is tabbed 
+    * so as to be properly formatted for adding to a standard format 
+    * java class. 
+    * @param identifier
+    * @param returnType
+    * @return 
+    */
+   public String writeFileFormattedGetter(String identifier, String returnType)
+   {
+      String getterString = "";
+      
+      
+      getterString += "\tpublic " + returnType + " get" + 
+              identifier.substring(0,1).toUpperCase() + identifier.substring(1)
+              + "()" + "\n\t{\n\t\treturn " + identifier + ";" + "\n\t}";
+      
+      return getterString;
+   }
 }
