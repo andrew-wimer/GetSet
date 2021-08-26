d07222798d7b447bbb2d1beeb3f167620a1406bc
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-25 22:39:16 -0400

    working version, now can handle 1) backing out of menu  2) printing to java files and to the terminal

--- src/getset/JavaGSBranch.java
+++ src/getset/JavaGSBranch.java
@@ -1,7 +1,7 @@
 /**
 * PROGRAMMED BY: Andrew Wimer
 * CREATED ON: Aug 23 2021
-* LAST UPDATE: Aug 24 2021
+* LAST UPDATE: Aug 25 2021
 */
 
 package getset;
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
  * CLASS DESCRIPTION: Handles inputs, submenus, and program functioning 
  * when java is selected as the language to generate getters and setters.
  * @author Andrew Wimer
- * 
+ * Aug 25 2021
  */
 public class JavaGSBranch implements GetSetBranch {
 
@@ -28,10 +28,23 @@ public class JavaGSBranch implements GetSetBranch {
          languageMainMenu();
       } catch (IOException ex) {
          Logger.getLogger(JavaGSBranch.class.getName()).log(Level.SEVERE, null, ex);
-      }
-      
-      
-      
+      }  
+   }
+   
+   /**
+    * Displays the selection of options available in the main menu for
+    * importing Java data members. 
+    */
+   public void displayLanguageMainMenu()
+   {
+      System.out.println("Java Main Menu");
+         System.out.println("Choose method of importing data members: ");
+         System.out.print("[c] Input to console\n");
+         System.out.print("[j] Import from Java file \n");
+         System.out.print("[l] Import from list in text file \n");
+         System.out.print("[b] Back \n");
+         System.out.print("[x] Exit program\n");
+         System.out.print("\nEnter Menu Option: ");
    }
    
    /**
@@ -42,79 +55,76 @@ public class JavaGSBranch implements GetSetBranch {
    public void languageMainMenu() throws IOException {
       BufferedReader reader = 
               new BufferedReader(new InputStreamReader(System.in));
-      
       String choice = "";
       do
-      {
-         System.out.println("Java Main Menu");
-         System.out.println("Choose method of importing data members: ");
-         System.out.print("[c] Input to console\n");
-         System.out.print("[j] Import from Java file \n");
-         System.out.print("[l] Import from list in text file \n");
-         System.out.print("[b] Back \n");
-         System.out.print("[x] Exit\n");
-         System.out.print("\nEnter Menu Option: ");
-
-         
-         
+      {     
+         displayLanguageMainMenu();
          choice = reader.readLine();
-         
-         
-
          switch(choice.toLowerCase()){
             
             case "j":
                importFromJavaFile();
-            case "b":
                break;
             case "x": 
-               System.out.println("Closing program...");
-               System.exit(0);
+               closeProgram();
+               break;
+            case "b":
+               break;
             default:
             System.out.println("Invalid choice; try again.");
                   break;
          }
       } while (!choice.equalsIgnoreCase("x")
               && !choice.equalsIgnoreCase("b"));
-      
-     reader.close();
    }
-
-   @Override
-   public void outputSelection(DataMemberMap dMM) throws IOException {  
-      
-      String choice = "";
-      do
-      {
-         System.out.println("Output Selection Menu");
+   
+   /**
+    * Displays the selection of options available in the Output Selection
+    * menu
+    */
+   public void displayOutputSelectionMenu()
+   {
+      System.out.println("Output Selection Menu");
          System.out.println("Choose method of outputting data members: ");
          System.out.print("[f] Print to file\n");
          System.out.print("[t] Print to terminal\n");
          System.out.print("[c] Copy to clipboard\n");
+         System.out.print("[b] Back \n");
          System.out.print("[x] Exit\n");
          System.out.print("\nEnter Menu Option: ");
+   }
 
-         BufferedReader reader = 
+   /**
+    * Output selection menu for outputting getters and setters from the
+    * processed data members.
+    * @throws IOException 
+    */
+   @Override
+   public void outputSelectionMenu(DataMemberMap dMM) throws IOException {  
+      BufferedReader reader = 
               new BufferedReader(new InputStreamReader(System.in));
-         
-         choice = reader.readLine();
 
-               reader.close();
+      String choice = "";
+      do
+      {  
+         displayOutputSelectionMenu();
+         choice = reader.readLine();
          
-         switch(choice.toLowerCase()){
-            
+         switch(choice.toLowerCase()){          
             case "f":
                printToFile(dMM);
+               break;
+            case "b":
+               break;
             case "x": 
-               System.out.println("Closing program...");
-               System.exit(0);
+               closeProgram();
+               break;
             default:
             System.out.println("Invalid choice; try again.");
                   break;
          }
-      } while (!choice.equalsIgnoreCase("x"));
-      
-
+      } while (!choice.equalsIgnoreCase("x")
+              && !choice.equalsIgnoreCase("b"));
    }
    
    /**
@@ -128,8 +138,7 @@ public class JavaGSBranch implements GetSetBranch {
        DataMemberParser dMP = new JavaDataMemberParser();
        DataMemberMap dMM = new DataMemberMap();
        dMM = dMP.parseFile(filePath);
-       outputSelection(dMM);  
-       
+       outputSelectionMenu(dMM);  
    }
    
    /**
@@ -145,8 +154,14 @@ public class JavaGSBranch implements GetSetBranch {
       MethodQueue[] getsSets = new LinkedMethodQueue[2];
       getsSets = mTM.generateMethodQueues(dMM);
       mPrint.printToFile(getsSets[0], getsSets[1], filePath);
+      System.out.println("Methods printed to file: " +  filePath);
    }
    
+   /**
+    * Method for user to input the path of a file.
+    * @return
+    * @throws IOException 
+    */
    public String getFilePath() throws IOException
    {
       BufferedReader reader = 
@@ -154,8 +169,41 @@ public class JavaGSBranch implements GetSetBranch {
        System.out.println("Enter your filepath: ");
        String filePath = "";
        filePath = reader.readLine();
-       reader.close();
        return filePath;
    }
+   
+   /**
+    * Closes the GetSet program.
+    */
+   public void closeProgram()
+   {
+      System.out.println("Closing program...");
+      System.exit(0);
+   }
+   
+  /**
+    * Outputs getters and setters to console
+    * @throws FileNotFoundException
+    * @throws IOException 
+    */
+   public void printToConsole(DataMemberMap dMM)
+   {
 
+      MethodPrinter mPrint = new JavaMethodPrinter();
+      MethodQueue[] getsSets = dataMembersToQueue(dMM);
+      mPrint.printToConsole(getsSets[0], getsSets[1]);
+   }
+ 
+   /**
+    * Creates array of getter and setter queues from the mapping of
+    * identifiers to return types in the DataMemberMap. 
+    * @throws FileNotFoundException
+    * @throws IOException 
+    */
+   public MethodQueue[] dataMembersToQueue(DataMemberMap dMM)
+   {
+        MembersToMethods mTM = new JavaMemsToMethods();
+      return mTM.generateMethodQueues(dMM);
+      
+   }
 }
