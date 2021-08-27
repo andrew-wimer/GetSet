9e55e91809305df39e1a7c732bbd408925e60f04
andrew-wimer <60301751+andrew-wimer@users.noreply.github.com>
2021-08-26 21:51:57 -0400

    changed from interface to abstract method, implemented closeProgram and getFilePath methods

--- src/getset/GetSetBranch.java
+++ src/getset/GetSetBranch.java
@@ -1,12 +1,15 @@
 /**
 * PROGRAMMED BY: Andrew Wimer
 * CREATED ON: Aug 23 2021
-* LAST UPDATE:  Aug 25 2021
+* LAST UPDATE:  Aug 26 2021
 */
 
 package getset;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * CLASS DESCRIPTION: Handles inputs, submenus, and program functioning 
@@ -15,16 +18,38 @@ import java.io.IOException;
  * @author Andrew Wimer
  * 
  */
-public interface GetSetBranch {
+public abstract class GetSetBranch {
    
-   public void run();
+   abstract public void run() throws IOException, FileNotFoundException;
    
-   public void languageMainMenu() throws IOException;
+   abstract public void printToConsole(DataMemberMap dMM);
    
-   public void outputSelectionMenu(DataMemberMap dMM) throws IOException;
+   abstract public void printToFile(DataMemberMap dMM) throws FileNotFoundException, 
+           IOException;
    
-   public String getFilePath() throws IOException;
+   abstract public MethodQueue[] dataMembersToQueue(DataMemberMap dMM);
    
-   public void closeProgram();
-
+   /**
+    * Closes the GetSet program.
+    */
+   protected void closeProgram()
+   {
+      System.out.println("Closing program...");
+      System.exit(0);
+   }
+   
+   /**
+    * Method for user to input the path of a file.
+    * @return
+    * @throws IOException 
+    */
+   protected String getFilePath() throws IOException
+   {
+      BufferedReader reader = 
+              new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("Enter your filepath: ");
+       String filePath = "";
+       filePath = reader.readLine();
+       return filePath;
+   }
 }
